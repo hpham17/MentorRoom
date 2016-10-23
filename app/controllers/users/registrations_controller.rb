2bfa669120115ffc9570ae5239e9d92696a5c6b8
Hubert Pham <hubqwerty@berkeley.edu>
2016-10-22 23:31:47 -0700

    fixed login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create
-    @user = User.new user_params
-    if @user.save
-      flash[:notice] = "Sign up successful."
-      redirect_to '/users'
-    else
-      flash[:danger] = "Failed to sign up."
-      render 'new'
-    end
+  before_filter :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
 end
