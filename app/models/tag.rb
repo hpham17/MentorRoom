d186b18b177edf0f7ae996a6713b913dc6cd0bba
Hubert Pham <hubqwerty@berkeley.edu>
2017-02-19 15:47:12 -0800

    updated tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,14 @@
 class Tag
-  Skills = ["Software Engineering", "Finance", "BioTechnology", "Investment Banking", "Politics", "Accounting"].freeze
+  SKILLS = ["Academia/Research", "Accounting", "Architecture", "Art", "Biotechnology", "Business Development", "Data Analytics", "Design", "Education", "Environmental Science", "Finance", "Government", "Hospitality", "Human Resources", "Investment Banking",
+  "Law", "Marketing", "Mechanical Engineering", "Non-Profit", "Performing Arts", "Photography", "Public Relations", "Product Management", "Social Media", "Software Engineering", "Politics"]
 
-  Helps = ["Academia vs. Industry", "Applying for grad / professional school", "Balancing family and career", "Career / Industry Trends", "Choosing a Major", "Mock Interviews", "Networking / Introductions to others in my field", "Resume / CV Review", "Career Planning", "Transitioning to life after College"].freeze
+  HELPS = ["Academia vs Industry", "Applying for grad school", "Career / Industry Trends", "Choosing a major", "Getting an internship", "Mock Interviews", "Networking", "Resume building", "Career Planning", "Navigating the PhD"]
 
   def self.is_skill?(skill)
-     Skills.include? skill
+     SKILLS.include? skill
   end
 
   def self.is_help?(help)
-     Helps.include? help
+     HELPS.include? help
   end
 end
