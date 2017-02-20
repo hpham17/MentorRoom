e5a298dfa4f35b2b6ea96bec01b7736606e9c310
Hubert Pham <hubqwerty@berkeley.edu>
2017-02-20 12:25:27 -0800

    new navbar icons

---
+++ db/migrate/20170220021838_create_activities.rb
@@ -0,0 +1,10 @@
+class CreateActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :activities do |t|
+      t.integer :user_id
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
