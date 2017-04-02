b2b2bdc5618f041cd17960a979055e3aad7063d0
Hubert Pham <hubqwerty@berkeley.edu>
2017-04-02 00:42:07 -0700

    fix gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'mentorroom.heroku.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => 'mail.google.com',
+    :port           => 587,
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
